5c89257e33f25da3a033c7e9f46de6d7ef837177
FabricioSilvany <fabriciosilvany57@gmail.com>
2025-08-11 14:00:30 -0300

    Exemplo01.java

---
+++ Exemplo01.java
@@ -0,0 +1,9 @@
+public class Exemplo{
+
+    public static void main(String[] args) {
+        String nome="Ruptor";
+        int idade=20;
+
+        System.out.println("Olá "+nome+" você tem "+idade+" anos");
+    }
+}
