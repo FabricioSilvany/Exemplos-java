eb0595798e1ff2c7ef17db979bde159d851125e7
FabricioSilvany <fabriciosilvany57@gmail.com>
2025-08-11 14:04:05 -0300

    Exemplo02.java

---
+++ Exemplo02.java
@@ -0,0 +1,19 @@
+public class Exemplo02 {
+    public static void main(String[] args) {
+        
+        int a=5;
+        int b=5;
+
+        int soma = a+b;
+        int sub = a-b;
+        int mult = a*b;
+        int divisao = a/b;
+
+        System.out.println("Resultados: ");
+        System.out.println("soma: "+soma);
+        System.out.println("subtração: "+sub);
+        System.out.println("multiplicação: "+mult);
+        System.out.println("divisão: "+divisao);
+
+    }
+}
